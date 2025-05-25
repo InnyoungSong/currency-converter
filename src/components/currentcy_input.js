e3f0ab0e6d81ce5e770306188e0468b8a330c48c
InnyoungSong <innyoungsong@gmail.com>
2025-05-24 21:23:48 -0500

    first commit

---
+++ src/components/currentcy_input.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+class CurrencyInput extends React.Component {
+    render() {
+        const {value, handleChange} = this.props;
+        return <input value={value} onChange={handleChange} type="number" />
+    }
+}
+
+export default CurrencyInput;
