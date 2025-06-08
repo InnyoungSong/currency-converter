058e6a0222475c65890c4970e74e7a67e7e89969
InnyoungSong <innyoungsong@gmail.com>
2025-06-08 10:34:55 -0500

    2.0

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import react from 'react';
 import './App.css';
 import currency_input from '../src/components/currency_input';
@@ -75,3 +76,45 @@ class CurrencyConverter extends react.Component {
 const container = document.getElementById('root');
 const root = React.DOM.createRoot(container);
 root.render(<CurrentcyConverter />)
+=======
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import gitHubIcon from './icons/github-brands.svg';
+import linkedInIcon from './icons/linkedin-in-brands.svg';
+import EasyAccessCurrency from './components/EasyAccessCurrency'; 
+
+import './App.css';
+import CurrencyHandler from './components/CurrencyHandler';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class='navbar navbar-light bg-light'>
+          <h2 class="navbar-brand mb-0 h1">Currency Converter</h2>
+      </nav>
+
+      <div className="content container mt-5">
+        <CurrencyHandler />
+
+        <div id='easyAccess'>
+          <h2>Exchange rates at a quick glance</h2>
+          <span>Base currency - 1 USD</span>
+          <div id='easyAccessCurrencies'>
+            <EasyAccessCurrency currency='CAD'/>
+            <EasyAccessCurrency currency='KRW'/>
+            <EasyAccessCurrency currency='EUR'/>
+            <EasyAccessCurrency currency='JPY'/>
+          </div>
+        </div>
+      </div>
+
+      <footer class="bg-dark text-light">
+            <a href='https://github.com/InnyoungSong'><img src={gitHubIcon} alt="gitHubIcon" width="24" height="24" /></a>
+            <a><img src={linkedInIcon} alt="gitHubIcon" width="24" height="24" /></a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 4387f08 (2.0)
